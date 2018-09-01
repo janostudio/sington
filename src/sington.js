c0702127f1796811a97df2afb93bb7b6ce7b4983
jiang <qinghuangniao@gmail.com>
2018-09-01 23:09:47 +0800

    开始学习jpg编码

--- src/sington.js
+++ src/sington.js
@@ -2,10 +2,13 @@
 import { bmpToPixelData } from './transferBmp';
 // dcm转换器
 import { dcmToPixelData } from './transferDcm';
+// jpg转换器
+import { jpgToPixelData } from './transferJpg';
 import version from './version';
 
 export default {
   bmpToPixelData,
   dcmToPixelData,
+  jpgToPixelData,
   version
 };
