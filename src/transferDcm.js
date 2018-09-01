0850de93bdab17bca9eb4ed4625830bed3387253
jiang <qinghuangniao@gmail.com>
2018-09-01 21:02:06 +0800

    完成对显式VR小端dcm图片的解析

--- src/transferDcm.js
+++ src/transferDcm.js
@@ -1,3 +1,8 @@
+/**
+ * 目前只做了对 显式VR小端（1.2.840.10008.1.2.1）的匹配
+ * 隐式VR小端（1.2.840.10008.1.2）暂未匹配
+ * 显式VR大端（1.2.840.10008.1.2.2）暂未匹配
+ */
 import utils from './utils';
 
 // 获取dcm中所有的key与value
@@ -34,7 +39,25 @@ function getMeta(unit8s) {
   }
 }
 
-// 将dcm的影像数据转成像素数据
+/*
+(0028,0002)        Samples per Pixel          
+(0028,0004)        Photometric Interpretation  
+(0028,0010)        Rows                        
+(0028,0011)        Columns                     
+(0028,0030)        Pixel Spacing               
+(0028,0100)        Bits Allocated              
+(0028,0101)        Bits Stored                 
+(0028,0102)        High Bit                    
+(0028,0103)        Pixel Representation        
+(0028,1050)        Window Center               
+(0028,1051)        Window Width                
+(0028,1052)        Rescale Intercept           
+(0028,1053)        Rescale Slope              
+(0028,2110)        Lossy Image Compression     
+(0028,2112)        Lossy Image Compression Ratio
+将dcm的影像数据转成像素数据
+*/
+
 
 // dcm转pixelData
 export function dcmToPixelData(buffer) {
