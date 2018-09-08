23537659e833f80a9403406aba87d5b65b4eaeee
jiang <qinghuangniao@gmail.com>
2018-09-08 08:13:29 +0800

    dist

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,16 +1,16 @@
 const webpack = require('webpack');
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const baseConfig  = require('./webpack.base.conf');
+
+baseConfig.entry = './src/sington.js';
+baseConfig.output.filename = 'sington.min.js';
+baseConfig.plugins = [];
 
 baseConfig.plugins.push[
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: '"production"'
-    }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
     }
   }),
   new webpack.LoaderOptionsPlugin({
@@ -19,5 +19,15 @@ baseConfig.plugins.push[
 ]
 
 module.exports = {
-  ...baseConfig
+  ...baseConfig,
+  //压缩js
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: false
+        }
+      })
+    ]
+  },
 }
